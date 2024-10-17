a7cb07008413129c9cd8a3b1e023e19257b63fe4
MarianaBMoraes <marianabmoraes.ef@gmail.com>
2024-10-17 20:46:53 -0300

    feat

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const connectDB = async() => {
+const connectDB = async () => {
     try {
         await mongoose.connect(
-            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`
-            // `mongoose.connect('mongodb+srv://<DB_USER>:<PASSWORD>i@<CLUSTER_NAME>/<COLLECTION_NAME>');`
-          );
+            `mongodb+srv://mariana_bmoraes:mariana@cluster0.kqjgr.mongodb.net/Oficina`
+        );
+        console.log('Conectado ao MongoDB');
 
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB', error);
